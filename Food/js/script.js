b4e389a82731cbe104c7052f306235a2839cafb9
Tatiana <tanianova@yandex.ru>
2021-12-19 14:31:41 +0300

    send data from forms to server

--- Food/js/script.js
+++ Food/js/script.js
@@ -215,4 +215,56 @@ window.addEventListener('DOMContentLoaded', () => {
         ".menu .container",
         "menu__item"
     ).render();
+
+    //forms
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'спасибо,мы с вами скоро свяжемся',
+        failure: 'Что-то пошло не так '
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function (value, key) {
+                object[key] = value;
+            });
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
 });
